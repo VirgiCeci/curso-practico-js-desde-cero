b8b9a7e11018993a7e33410ed5f15cf1388af752
Cecilia Kletzl <virginiacecilia@gmail.com>
2022-09-04 01:03:22 -0300

    nuevo commit a la una cero 3 de la mañana

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("Hola!! Aquí estamos practicando y modificando!!!--. Ahora siendo las 23:53 agrego otro texto para probar. Nuevo cambio a ver qué pasa.........")
+console.log("Hola, estoy probando nuevamente")
